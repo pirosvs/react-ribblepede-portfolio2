edb221fb31ab3b8f366c28ea124fe36dc73c3f4c
darmstrong <armstrong2579@gmail.com>
2022-08-24 21:16:21 -0700

    readd pages files

---
+++ ribblepede-portfolio/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Damien Armstrong is a student and aspiring Web Developper living and working out of the Bay Area. He is deeply appreciative of the support of his friends
+        and partner during hir learning process with the UC Berkely coding bootcamp. And lucky for you, he is oh so very employable! Check out hir work on 
+        <a href="https://github.com/pirosvs">Github </a> and <a href="https://www.linkedin.com/in/damien-armstrong-412319138/"> Linkedin</a>.
+        </p>
+
+        <h2>Fun Facts</h2>
+        <p>They have a sweet (but bitey!) cat Penny -- In his spare time he likes to find new queer and trans 
+        comics and make tailored suggestions to hir friends -- Ze's fallen down the rhythm game rabbit hole, currently spending
+        a truly outstanding amount of time on Project DIVA: Mega Mix and Future Tone
+        </p>
+        <img src="../../images/aboutimg.JPG" alt="Damien, a short white 'boy' and owner of this site, sits on some rocks in a river with trees in
+            the background. He has light blue glasses and a mask, a black jean jacket, pants, and boots, and a red shirt with a heart web design
+            on it. There are some pins on the jacket and a crystal necklace."/>
+    </div>
+  );
+}
