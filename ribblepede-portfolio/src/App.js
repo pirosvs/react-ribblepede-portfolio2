cad3b582beaf8c6e58d80015a5bd8536a50e4ef7
darmstrong <armstrong2579@gmail.com>
2022-08-25 22:48:32 -0700

    fix font size

--- ribblepede-portfolio/src/App.js
+++ ribblepede-portfolio/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import PortfolioContainer from "./components/Portfolio";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Project from "./components/Project";
 const App = () => <PortfolioContainer />;
 
 export default App;
