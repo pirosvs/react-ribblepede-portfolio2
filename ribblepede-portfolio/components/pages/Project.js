1f2e78b8870a938b7e7956e27686a385399f7a86
darmstrong <armstrong2579@gmail.com>
2022-08-25 23:17:32 -0700

    fix columns

--- ribblepede-portfolio/components/pages/Project.js
+++ ribblepede-portfolio/components/pages/Project.js
@@ -19,30 +19,30 @@ export default function Project() {
             <img class="" src={NarratoriumImg}
             title="Narratorium" alt="Homepage with description of the site and story"/></a>
         </p>
-        <p className="1/3 col space">
+        <p className="col-sm-12 col-md-6 col-lg-3">
         <h2>Movie Suggestor</h2>
         <a href="https://fiaschettima.github.io/MovieSuggestion-Proj1">
             <img class= "project-img" src={MovieSuggestGif}
             title="Movie Suggestions" alt="Movie suggestion website"/></a>
         </p>
-        <p className="1/3 col space">
+        <p className="col-sm-12 col-md-6 col-lg-3">
             <h2>E-Commerce Back-End</h2>
             <a href="https://github.com/pirosvs/ecommerce-izuchi"><img class="" src={EcommerceImg}
             title="Ecommerce Backend" alt="Homepage with description of the site and story"/></a>
         </p>
-        <p className="1/3 col space">
+        <p className="col-sm-12 col-md-6 col-lg-3">
         <h2>Password Generator</h2>
             <a href="https://pirosvs.github.io/password-generator/">
             <img class="project-img" src={PassGenImg}
             title="Password Generator" alt="Password generator with a randomized password"/></a>
         </p>
-        <p className="1/3 col space">
+        <p className="col-sm-12 col-md-6 col-lg-3">
             <h2>Coding Quiz</h2>
             <a href="https://pirosvs.github.io/code-quiz/">
             <img class= "project-img" src={CodeQuizImg}
             title="Code Quiz" alt="Code quiz question example"/></a>        
         </p>
-        <p className="1/3 col space">
+        <p className="col-sm-12 col-md-6 col-lg-3">
         <h2>Scheduler</h2>
             <a href="https://pirosvs.github.io/scheduler">
             <img class= "project-img" src={SchedulerImg}
