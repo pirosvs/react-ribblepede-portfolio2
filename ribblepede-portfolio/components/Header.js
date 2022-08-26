e96b8b64ce672f7735d407c7526d3ab81229162b
darmstrong <armstrong2579@gmail.com>
2022-08-25 22:20:53 -0700

    add basic header

---
+++ ribblepede-portfolio/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Header.css';
+
+function Header() {
+    return (
+      <header style={styles} className="header">
+        <h1 style={styles}>Damien Armstrong</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
